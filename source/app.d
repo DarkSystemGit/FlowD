0aed12a04d23ca7b6158411af4e72d62a8e5a4a6
DarkSystemGit <85317259+DarkSystemGit@users.noreply.github.com>
2024-08-17 16:54:05 -0400

    audio

--- source/app.d
+++ source/app.d
@@ -1,25 +1,35 @@
 import std.stdio;
 import std.format;
 import std.string;
-import text;
+import audio;
 import raylib;
 import raymath;
 
 void main()
 {
+	class PitchRise:AudioEffect{
+		void onFrame(AudioAsset audio){
+			audio.pitch+=0.001;
+		}
+	}
 	SetTargetFPS(60);
 	InitWindow(800, 640, "Flow.D");
+	InitAudioDevice();
 	scope (exit)
+	{
+		CloseAudioDevice();
 		CloseWindow();
-	
-	
-	while (!WindowShouldClose()){
+	}
+	AudioAsset audio = new AudioAsset();
+	audio.load("source/mario.wav");
+	PitchRise audioEffect=new PitchRise();
+	audio.addEffect(audioEffect);
+	audio.play();
+	while (!WindowShouldClose())
+	{
 		BeginDrawing();
 		ClearBackground(Colors.BLACK);
-		
+		audio.onFrame();
 		EndDrawing();
 	}
 }
-
-
-
--- source/audio.d
+++ source/audio.d
@@ -3,4 +3,78 @@ import raylib;
 import model;
 import std.stdio;
 import std.string;
-class AudioAsset:Asset{}
+class AudioAsset{
+    Music audio;
+    float pitch=1.0;
+    float volume=1.0;
+    float time=0.0;
+    float pan=0.0;
+    bool looping=false;
+    AudioEffect[] effects=new AudioEffect[0];
+    void load(string path){
+        this.audio=LoadMusicStream(path.toStringz());
+        this.audio.looping=false;
+    }
+    void onFrame(){
+        UpdateMusicStream(this.audio);
+        this.time=GetMusicTimePlayed(this.audio);
+        this.setPitch(this.pitch);
+        this.setVolume(this.volume);
+        this.setPan(this.pan);
+        this.setLooping(this.looping);
+        for(int i=0;i<this.effects.length;i++){
+            this.effects[i].onFrame(this);
+        }
+    }
+    void play(){
+        PlayMusicStream(this.audio);
+    }
+    void pause(){
+        PauseMusicStream(this.audio);
+    }
+    void resume(){
+        ResumeMusicStream(this.audio);
+    }
+    void stop(){
+        StopMusicStream(this.audio);
+    }
+    void setPitch(float pitch){
+        this.pitch=pitch;
+        SetMusicPitch(this.audio,pitch);
+    }
+    void setPosition(float time){
+        SeekMusicStream(this.audio,time);
+    }
+    void setVolume(float volume){
+        this.volume=volume;
+        SetMusicVolume(this.audio,volume);
+    }   
+    void setPan(float pan){
+        this.pan=pan;
+        SetMusicPan(this.audio,pan);
+    }
+    void setLooping(bool loop){
+        this.looping=loop;
+        this.audio.looping=loop;
+    }
+    bool playing(){
+        return IsMusicStreamPlaying(this.audio);
+    }
+    float getTime(){
+        return GetMusicTimePlayed(this.audio);
+    }
+    float getLength(){
+        return GetMusicTimeLength(this.audio);
+    }
+    void addEffect(AudioEffect effect){
+        this.effects.length++;
+        this.effects[this.effects.length-1]= effect;
+        
+    }
+    ~this(){
+        UnloadMusicStream(this.audio);
+    } 
+}
+interface AudioEffect{
+    void onFrame(AudioAsset audio);
+}
